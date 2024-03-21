77b07250a041794051483e575c7f21ea0e8be3d3
bipro <biprocuet@gmail.com>
2024-03-21 13:13:14 +0600

    jwtToken validation

---
+++ src/main/java/com/bipro/food/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.bipro.food.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="afsdfsdfsdfdsfwerwerwerwerewhagkfxczvxvhscbhjsbdchsdbkf";
+
+    public static final String JWT_HEADER ="Authorization";
+
+}
